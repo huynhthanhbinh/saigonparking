6ae97e08759fa30b1f6e71c6cf632f829753bb65
huynhthanhbinh <1653006@student.hcm.edu.vn>
2020-02-16 17:23:44 +0700

    [SERVER] init API

---
+++ API/src/main/java/com/bht/parkingmap/ParkingMapServerConfiguration.java
@@ -0,0 +1,10 @@
+package com.bht.parkingmap;
+
+/**
+ *
+ * @author bht
+ */
+public final class ParkingMapServerConfiguration {
+    public static final String HOST = "localhost";
+    public static final Integer PORT = 9999;
+}
