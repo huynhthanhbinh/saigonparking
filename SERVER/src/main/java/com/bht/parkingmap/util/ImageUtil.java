7ba7a24dfd64ace5c2358d65014955400b228cbc
huynhthanhbinh <1653006@student.hcm.edu.vn>
2020-02-23 14:28:34 +0700

    [SERVER] update ImageUtil

--- SERVER/src/main/java/com/bht/parkingmap/util/ImageUtil.java
+++ SERVER/src/main/java/com/bht/parkingmap/util/ImageUtil.java
@@ -9,6 +9,8 @@ import java.nio.file.Files;
 import javax.imageio.ImageIO;
 import javax.validation.constraints.NotNull;
 
+import org.apache.log4j.Level;
+
 import com.bht.parkingmap.configuration.AppConfiguration;
 
 /**
@@ -33,16 +35,46 @@ public final class ImageUtil {
     }
 
 
-    public static byte[] getImage(@NotNull String pathFromGalleryDir, @NotNull ImageExtension fileExtension) throws IOException {
-        return Files.readAllBytes(new File(AppConfiguration.RESOURCE_DIR +
-                "/gallery/" + pathFromGalleryDir + '.' + fileExtension.extension).toPath());
+    private static String toImagePath(@NotNull String pathFromGalleryDir, @NotNull ImageExtension fileExtension) {
+        return AppConfiguration.RESOURCE_DIR + "/gallery/" + pathFromGalleryDir + '.' + fileExtension.extension;
     }
 
 
-    public static void saveImage(@NotNull byte[] imageData, @NotNull String pathFromGalleryDir, @NotNull ImageExtension fileExtension) throws IOException {
+    public static byte[] getImage(@NotNull String pathFromGalleryDir, @NotNull ImageExtension fileExtension) throws IOException {
+        File image = new File(toImagePath(pathFromGalleryDir, fileExtension));
+        return (image.exists()) ? Files.readAllBytes(image.toPath()) : null;
+    }
+
+
+    public static boolean saveOrUpdateImage(byte[] imageData, @NotNull String pathFromGalleryDir, @NotNull ImageExtension fileExtension) throws IOException {
+        if (imageData != null) {
+            File image = new File(toImagePath(pathFromGalleryDir, fileExtension));
+            deleteImage(image);
+            LoggingUtil.log(Level.INFO, "GALLERY", "Save/Update", image.getPath());
+            return saveImage(imageData, image, fileExtension);
+        }
+
+        LoggingUtil.log(Level.WARN, "GALLERY", "Save/Update", "FAIL due to empty content !");
+        return true;
+    }
+
+
+    private static boolean saveImage(@NotNull byte[] imageData, @NotNull File image, @NotNull ImageExtension fileExtension) throws IOException {
         ByteArrayInputStream inputStream = new ByteArrayInputStream(imageData);
         BufferedImage bImage = ImageIO.read(inputStream);
-        ImageIO.write(bImage, fileExtension.extension, new File(AppConfiguration.RESOURCE_DIR +
-                "/gallery/" + pathFromGalleryDir + '.' + fileExtension.extension));
+        return ImageIO.write(bImage, fileExtension.extension, image);
+    }
+
+
+    public static void deleteImage(@NotNull String pathFromGalleryDir, @NotNull ImageExtension fileExtension) throws IOException {
+        deleteImage(new File(toImagePath(pathFromGalleryDir, fileExtension)));
+    }
+
+
+    private static void deleteImage(@NotNull File image) throws IOException {
+        if (image.exists()) {
+            Files.delete(image.toPath());
+            LoggingUtil.log(Level.INFO, "GALLERY", "Delete", image.getPath());
+        }
     }
 }
