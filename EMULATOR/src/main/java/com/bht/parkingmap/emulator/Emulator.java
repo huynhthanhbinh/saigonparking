0f987eeaa45c62861ae52424ff1f97b5152f258d
huynhthanhbinh <1653006@student.hcm.edu.vn>
2020-03-04 03:58:39 +0700

    [SERVER] update query

--- EMULATOR/src/main/java/com/bht/parkingmap/emulator/Emulator.java
+++ EMULATOR/src/main/java/com/bht/parkingmap/emulator/Emulator.java
@@ -10,6 +10,6 @@ import lombok.extern.log4j.Log4j;
 public class Emulator {
 
     public static void main(String[] args) {
-        log.info("This is the emulator appication");
+        log.info("This is the emulator application");
     }
 }
