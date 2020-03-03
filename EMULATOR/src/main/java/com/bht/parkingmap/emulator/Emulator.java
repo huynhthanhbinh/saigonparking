e70a9d099c82edbab00658b090b3774c468c327f
huynhthanhbinh <1653006@student.hcm.edu.vn>
2020-03-03 23:31:55 +0700

    [SERVER] init emulator

---
+++ EMULATOR/src/main/java/com/bht/parkingmap/emulator/Emulator.java
@@ -0,0 +1,15 @@
+package com.bht.parkingmap.emulator;
+
+import lombok.extern.log4j.Log4j;
+
+/**
+ *
+ * @author bht
+ */
+@Log4j
+public class Emulator {
+
+    public static void main(String[] args) {
+        log.info("This is the emulator appication");
+    }
+}
