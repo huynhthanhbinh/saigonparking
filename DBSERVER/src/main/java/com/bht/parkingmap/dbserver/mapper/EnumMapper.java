130c84a9b7ad00d687e733410dd2793c02c0866a
huynhthanhbinh <1653006@student.hcm.edu.vn>
2020-03-08 17:46:28 +0700

    [SERVER] using Lombok setter with @Autowired annotation

--- DBSERVER/src/main/java/com/bht/parkingmap/dbserver/mapper/EnumMapper.java
+++ DBSERVER/src/main/java/com/bht/parkingmap/dbserver/mapper/EnumMapper.java
@@ -3,9 +3,12 @@ package com.bht.parkingmap.dbserver.mapper;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.validation.constraints.NotNull;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Named;
 import org.mapstruct.NullValueMappingStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.bht.parkingmap.api.proto.parkinglot.ParkingLotType;
@@ -18,25 +21,38 @@ import com.bht.parkingmap.dbserver.repository.user.UserRoleRepository;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 
-import lombok.AllArgsConstructor;
+import lombok.Setter;
 
 /**
+ * this class is self-customized mapper for all enums, include:
+ *      + UserRole:       3 role --> ADMIN, CUSTOMER, PARKING_LOT_EMPLOYEE
+ *      + ParkingLotType: 3 type --> PRIVATE, BUILDING, STREET
+ *
+ * for using repository inside Component class,
+ * we need to {@code @Autowired} it by Spring Dependency Injection
+ * we can achieve that easily
+ * by using {@code @Setter(onMethod = @__(@Autowired)} for class level like below
+ *
+ * we cannot use {@code @AllArgsConstructor} for class level,
+ * because these repository/injected fields are optional,
+ * and it will conflict with {@code @Mapper @Component} bean
+ * which will be initialized by NonArgsConstructor !!!!!!!!!
  *
  * @author bht
  */
 @Component
-@AllArgsConstructor
+@Setter(onMethod = @__(@Autowired), onParam = @__(@NotNull))
 @Mapper(componentModel = "spring", nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
 public abstract class EnumMapper implements BaseBean {
 
-    private final UserRoleRepository userRoleRepository;
-    private final ParkingLotTypeRepository parkingLotTypeRepository;
+    private UserRoleRepository userRoleRepository;
+    private ParkingLotTypeRepository parkingLotTypeRepository;
 
-    private static final Map<Long, UserRole> USER_ROLE_MAP = new HashMap<>(4);
-    private static final Map<Long, ParkingLotType> PARKING_LOT_TYPE_MAP = new HashMap<>(4);
+    private static final Map<Long, UserRole> USER_ROLE_MAP = new HashMap<>();
+    private static final Map<Long, ParkingLotType> PARKING_LOT_TYPE_MAP = new HashMap<>();
 
-    private static final BiMap<UserRoleEntity, UserRole> USER_ROLE_BI_MAP = HashBiMap.create(4);
-    private static final BiMap<ParkingLotTypeEntity, ParkingLotType> PARKING_LOT_TYPE_BI_MAP = HashBiMap.create(4);
+    private static final BiMap<UserRoleEntity, UserRole> USER_ROLE_BI_MAP = HashBiMap.create();
+    private static final BiMap<ParkingLotTypeEntity, ParkingLotType> PARKING_LOT_TYPE_BI_MAP = HashBiMap.create();
 
     @Override
     public void initialize() {
@@ -47,12 +63,12 @@ public abstract class EnumMapper implements BaseBean {
     }
 
     @Named("toUserRoleFromId")
-    public UserRole toUserRole(Long userRoleId) {
+    UserRole toUserRole(Long userRoleId) {
         return USER_ROLE_MAP.get(userRoleId);
     }
 
     @Named("toUserRole")
-    public UserRole toUserRole(UserRoleEntity userRoleEntity) {
+    UserRole toUserRole(UserRoleEntity userRoleEntity) {
         return USER_ROLE_BI_MAP.get(userRoleEntity);
     }
 
@@ -62,12 +78,12 @@ public abstract class EnumMapper implements BaseBean {
     }
 
     @Named("toParkingLotTypeFromId")
-    public ParkingLotType toParkingLotType(Long parkingLotTypeId) {
+    ParkingLotType toParkingLotType(Long parkingLotTypeId) {
         return PARKING_LOT_TYPE_MAP.get(parkingLotTypeId);
     }
 
     @Named("toParkingLotType")
-    public ParkingLotType toParkingLotType(ParkingLotTypeEntity parkingLotTypeEntity) {
+    ParkingLotType toParkingLotType(ParkingLotTypeEntity parkingLotTypeEntity) {
         return PARKING_LOT_TYPE_BI_MAP.get(parkingLotTypeEntity);
     }
 
@@ -79,28 +95,24 @@ public abstract class EnumMapper implements BaseBean {
     // initialize ======================================================================================================
 
     private void initUserRoleMap() {
-        USER_ROLE_MAP.put(0L, UserRole.UNRECOGNIZED);
         USER_ROLE_MAP.put(1L, UserRole.ADMIN);
         USER_ROLE_MAP.put(2L, UserRole.CUSTOMER);
         USER_ROLE_MAP.put(3L, UserRole.PARKING_LOT_EMPLOYEE);
     }
 
     private void initUserRoleBiMap() {
-        USER_ROLE_BI_MAP.put(userRoleRepository.getOne(0L), UserRole.UNRECOGNIZED);
         USER_ROLE_BI_MAP.put(userRoleRepository.getOne(1L), UserRole.ADMIN);
         USER_ROLE_BI_MAP.put(userRoleRepository.getOne(2L), UserRole.CUSTOMER);
         USER_ROLE_BI_MAP.put(userRoleRepository.getOne(3L), UserRole.PARKING_LOT_EMPLOYEE);
     }
 
     private void initParkingLotTypeMap() {
-        PARKING_LOT_TYPE_MAP.put(0L, ParkingLotType.UNRECOGNIZED);
         PARKING_LOT_TYPE_MAP.put(1L, ParkingLotType.PRIVATE);
         PARKING_LOT_TYPE_MAP.put(2L, ParkingLotType.BUILDING);
         PARKING_LOT_TYPE_MAP.put(3L, ParkingLotType.STREET);
     }
 
     private void initParkingLotTypeBiMap() {
-        PARKING_LOT_TYPE_BI_MAP.put(parkingLotTypeRepository.getOne(0L), ParkingLotType.UNRECOGNIZED);
         PARKING_LOT_TYPE_BI_MAP.put(parkingLotTypeRepository.getOne(1L), ParkingLotType.PRIVATE);
         PARKING_LOT_TYPE_BI_MAP.put(parkingLotTypeRepository.getOne(2L), ParkingLotType.BUILDING);
         PARKING_LOT_TYPE_BI_MAP.put(parkingLotTypeRepository.getOne(3L), ParkingLotType.STREET);
