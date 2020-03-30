bbe52d3a3f411c04b098a3c1feb498fe91d4091b
huynhthanhbinh <1653006@student.hcm.edu.vn>
2020-03-31 02:11:41 +0700

    [SERVER] update maven-docker-plugin config

---
+++ gateway-register.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# register parking lot service
+curl -XPOST localhost:8001/services \
+  --data name=parkinglot \
+  --data protocol=grpc \
+  --data host=appserver \
+  --data port=9999 \
+  --data connect_timeout=30000 \
+  --data write_timeout=30000 \
+  --data read_timeout=30000 \
+  #--client_certificate=
+
+# route parking lot service
+curl -XPOST localhost:8001/services/parkinglot/routes \
+  --data protocols=grpc \
+  --data name=catch-all-parking-lot \
+  --data paths=/com.bht.parkingmap.api.proto.parkinglot.ParkingLotService/
