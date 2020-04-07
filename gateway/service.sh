1ce205b55ba431c4879e9211602336eadee930c9
huynhthanhbinh <1653006@student.hcm.edu.vn>
2020-04-08 02:47:35 +0700

    [SERVER] enable interact with Kong Admin via localhost (binding port)

--- gateway/service.sh
+++ gateway/service.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
-# service host: dockerhost
 
-# register parking lot service
+# service host: 10.148.0.7
+APP_SERSER_HOST=10.148.0.7
+
+printf "\n\nRegister parking-lot service"
 curl -XPOST localhost:8001/services \
   --data name=parkinglot \
   --data protocol=grpc \
-  --data host=appserver \
+  --data host=${APP_SERSER_HOST} \
   --data port=9999 \
   --data connect_timeout=10000 \
   --data write_timeout=10000 \
   --data read_timeout=10000
 
-# route parking lot service
+printf "\n\nRegister parking-lot routing"
 curl -XPOST localhost:8001/services/parkinglot/routes \
   --data protocols=grpc \
   --data name=catch-all-parking-lot \
