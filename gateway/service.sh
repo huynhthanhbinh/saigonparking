0195e623ce56bd6c4d731edefc9bfdd7546aa2be
huynhthanhbinh <1653006@student.hcm.edu.vn>
2020-04-13 03:26:37 +0700

    [SERVER] register User Service in API Gateway

--- gateway/service.sh
+++ gateway/service.sh
@@ -1,20 +1,40 @@
 #!/bin/sh
+KONG_ADMIN_HOST=localhost
+KONG_ADMIN_PORT=8001
+CONNECT_TIMEOUT=10000
 
-# service host: 10.148.0.7
-APP_SERSER_HOST=10.148.0.7
+function registerService () {
+    printf "\n\nRegister %s service\n" $1
+    curl -XPOST ${KONG_ADMIN_HOST}:${KONG_ADMIN_PORT}/services \
+      --data name=$1 \
+      --data protocol=grpc \
+      --data host=$2 \
+      --data port=$3 \
+      --data connect_timeout=$4 \
+      --data write_timeout=$4 \
+      --data read_timeout=$4
+}
 
-printf "\n\nRegister parking-lot service"
-curl -XPOST localhost:8001/services \
-  --data name=parkinglot \
-  --data protocol=grpc \
-  --data host=${APP_SERSER_HOST} \
-  --data port=9999 \
-  --data connect_timeout=10000 \
-  --data write_timeout=10000 \
-  --data read_timeout=10000
+function registerServiceRoutes () {
+    printf "\n\nRegister %s service routes\n" $1
+    curl -XPOST ${KONG_ADMIN_HOST}:${KONG_ADMIN_PORT}/services/$1/routes \
+      --data protocols=grpc \
+      --data name=catch-all-$1-services \
+      --data paths=$2
+}
 
-printf "\n\nRegister parking-lot routing"
-curl -XPOST localhost:8001/services/parkinglot/routes \
-  --data protocols=grpc \
-  --data name=catch-all-parking-lot \
-  --data paths=/com.bht.parkingmap.api.proto.parkinglot.ParkingLotService/
+SERVICE_NAME=user
+SERVICE_HOST=10.148.0.7
+SERVICE_PORT=9999
+SERVICE_PATH=/com.bht.parkingmap.api.proto.user.UserService/
+
+registerService ${SERVICE_NAME} ${SERVICE_HOST} ${SERVICE_PORT} ${CONNECT_TIMEOUT}
+registerServiceRoutes ${SERVICE_NAME} ${SERVICE_PATH}
+
+SERVICE_NAME=parkinglot
+SERVICE_HOST=10.148.0.7
+SERVICE_PORT=9999
+SERVICE_PATH=/com.bht.parkingmap.api.proto.parkinglot.ParkingLotService/
+
+registerService ${SERVICE_NAME} ${SERVICE_HOST} ${SERVICE_PORT} ${CONNECT_TIMEOUT}
+registerServiceRoutes ${SERVICE_NAME} ${SERVICE_PATH}
