64f65dc013575882c99f0896f049892165074bb5
Huynh Thanh Binh <1653006@student.hcmus.edu.vn>
2020-05-24 04:08:00 +0700

    [SERVER] update emulator + maven pom

--- emulator/src/main/java/com/bht/saigonparking/emulator/Emulator.java
+++ emulator/src/main/java/com/bht/saigonparking/emulator/Emulator.java
@@ -7,14 +7,12 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.bht.saigonparking.api.grpc.auth.AuthServiceGrpc;
-import com.bht.saigonparking.api.grpc.auth.ValidateRequest;
-import com.bht.saigonparking.api.grpc.auth.ValidateResponse;
 import com.bht.saigonparking.api.grpc.parkinglot.ParkingLotInformation;
 import com.bht.saigonparking.api.grpc.parkinglot.ParkingLotServiceGrpc;
 import com.bht.saigonparking.api.grpc.user.User;
-import com.bht.saigonparking.api.grpc.user.UserRole;
 import com.bht.saigonparking.api.grpc.user.UserServiceGrpc;
 import com.bht.saigonparking.emulator.configuration.SpringApplicationContext;
+import com.google.protobuf.Int64Value;
 
 import io.grpc.StatusRuntimeException;
 import lombok.extern.log4j.Log4j2;
@@ -26,6 +24,7 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @EnableScheduling
 @SpringBootApplication
+@SuppressWarnings("all")
 public class Emulator extends SpringBootServletInitializer {
 
     @Override
@@ -48,37 +47,37 @@ public class Emulator extends SpringBootServletInitializer {
 
         ParkingLotInformation parkingLotInformation;
         User user;
-        ValidateResponse validateResponse;
+//        ValidateResponse validateResponse;
 
         try {
-            log.info("Start calling API");
-            validateResponse = authServiceBlockingStub.validateUser(ValidateRequest.newBuilder()
-                    .setUsername("htbinh")
-                    .setPassword("htbinh789")
-                    .setRole(UserRole.CUSTOMER)
-                    .build());
-            log.info("End calling API");
-            log.info("\n" + validateResponse);
+//            log.info("Start calling API");
+//            validateResponse = authServiceBlockingStub.validateUser(ValidateRequest.newBuilder()
+//                    .setUsername("htbinh")
+//                    .setPassword("htbinh789")
+//                    .setRole(UserRole.CUSTOMER)
+//                    .build());
+//            log.info("End calling API");
+//            log.info("\n" + validateResponse);
 
 //            log.info("Start calling API");
 //            parkingLotInformation = parkingLotServiceBlockingStub.getParkingLotById(Int64Value.of(1)).getInformation();
 //            log.info("End calling API");
 //            log.info("\n" + parkingLotInformation);
 
-//            log.info("Start calling API");
-//            user = userServiceBlockingStub.getUserById(Int64Value.of(1));
-//            log.info("End calling API");
-//            log.info("\n" + user);
+            log.info("Start calling API");
+            user = userServiceBlockingStub.getUserById(Int64Value.of(1));
+            log.info("End calling API");
+            log.info("\n" + user);
 
 //            log.info("Start calling API");
 //            parkingLotInformation = parkingLotServiceBlockingStub.getParkingLotById(Int64Value.of(3)).getInformation();
 //            log.info("End calling API");
 //            log.info("\n" + parkingLotInformation);
 
-//            log.info("Start calling API");
-//            user = userServiceBlockingStub.getUserById(Int64Value.of(4));
-//            log.info("End calling API");
-//            log.info("\n" + user);
+            log.info("Start calling API");
+            user = userServiceBlockingStub.getUserById(Int64Value.of(4));
+            log.info("End calling API");
+            log.info("\n" + user);
 
         } catch (StatusRuntimeException exception) {
 
--- emulator/src/main/java/com/bht/saigonparking/emulator/interceptor/AuthTokenProvideInterceptor.java
+++ emulator/src/main/java/com/bht/saigonparking/emulator/interceptor/AuthTokenProvideInterceptor.java
@@ -8,6 +8,7 @@ import io.grpc.ClientCall;
 import io.grpc.ClientInterceptor;
 import io.grpc.ForwardingClientCall;
 import io.grpc.Metadata;
+import io.grpc.Metadata.Key;
 import io.grpc.MethodDescriptor;
 
 /**
@@ -17,13 +18,15 @@ import io.grpc.MethodDescriptor;
 @Component
 public final class AuthTokenProvideInterceptor implements ClientInterceptor {
 
+    private static final String INTERNAL_KEY_NAME = "saigon-parking-internal";
+    private static final Key<String> INTERNAL_SERVICE_KEY = Key.of(INTERNAL_KEY_NAME, Metadata.ASCII_STRING_MARSHALLER);
+
     @Override
     public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(MethodDescriptor<ReqT, RespT> methodDescriptor, CallOptions callOptions, Channel channel) {
         return new ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(channel.newCall(methodDescriptor, callOptions)) {
             @Override
             public void start(Listener<RespT> responseListener, Metadata headers) {
-                headers.put(Metadata.Key.of("Authorization", Metadata.ASCII_STRING_MARSHALLER),
-                        "Huynh Thanh Binh");
+                headers.put(INTERNAL_SERVICE_KEY, "Saigon Parking Emulator");
                 super.start(responseListener, headers);
             }
         };
