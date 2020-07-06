dcc62a3ba0aacecf4f4b025b2b35b7aa1df70347
Huynh Thanh Binh <1653006@student.hcmus.edu.vn>
2020-07-06 14:15:16 +0700

    [SERVER] update rabbitmq definitions.json: add contact queue

--- common/src/main/java/com/bht/saigonparking/common/constant/SaigonParkingMessageQueue.java
+++ common/src/main/java/com/bht/saigonparking/common/constant/SaigonParkingMessageQueue.java
@@ -1,21 +1,24 @@
 package com.bht.saigonparking.common.constant;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 /**
  *
  * @author bht
  */
+@NoArgsConstructor(access = AccessLevel.NONE)
 public final class SaigonParkingMessageQueue {
-
-    private SaigonParkingMessageQueue() {
-    }
 
     public static final String TOPIC_EXCHANGE_NAME = "saigonparking.exchange";
 
     public static final String MAIL_QUEUE_NAME = "saigonparking.mail";
     public static final String USER_QUEUE_NAME = "saigonparking.user";
     public static final String PARKING_LOT_QUEUE_NAME = "saigonparking.parkinglot";
+    public static final String CONTACT_QUEUE_NAME = "saigonparking.contact";
 
     public static final String MAIL_TOPIC_ROUTING_KEY = "saigonparking.mail";
     public static final String USER_TOPIC_ROUTING_KEY = "saigonparking.user";
     public static final String PARKING_LOT_ROUTING_KEY = "saigonparking.parkinglot";
+    public static final String CONTACT_ROUTING_KEY = "saigonparking.contact";
 }
