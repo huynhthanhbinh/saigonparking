8f51d54f92b7ec98159d4e45d17fbeebd1986159
Huynh Thanh Binh <1653006@student.hcmus.edu.vn>
2020-07-13 21:27:05 +0700

    [SERVER] init BookingServiceImpl

---
+++ service/booking-service/src/main/java/com/bht/saigonparking/service/booking/service/main/impl/BookingServiceImpl.java
@@ -0,0 +1,15 @@
+package com.bht.saigonparking.service.booking.service.main.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bht.saigonparking.service.booking.service.main.BookingService;
+
+/**
+ *
+ * @author bht
+ */
+@Service
+@Transactional
+public class BookingServiceImpl implements BookingService {
+}
