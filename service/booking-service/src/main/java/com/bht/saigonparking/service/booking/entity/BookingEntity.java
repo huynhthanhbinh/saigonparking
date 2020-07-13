93201d8f0529ac15c111331ab5bb4a44f508b9fd
Huynh Thanh Binh <1653006@student.hcmus.edu.vn>
2020-07-13 20:54:10 +0700

    [SERVER] init all entities of BookingService

---
+++ service/booking-service/src/main/java/com/bht/saigonparking/service/booking/entity/BookingEntity.java
@@ -0,0 +1,33 @@
+package com.bht.saigonparking.service.booking.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.SelectBeforeUpdate;
+
+import com.bht.saigonparking.common.base.BaseEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+/**
+ *
+ * @author bht
+ */
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@SelectBeforeUpdate
+@Table(name = "[BOOKING]")
+public final class BookingEntity extends BaseEntity {
+}
