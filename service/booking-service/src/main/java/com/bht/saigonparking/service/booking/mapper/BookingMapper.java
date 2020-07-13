d612b3ec9f679eae45b1817e57ec2d763af7286a
Huynh Thanh Binh <1653006@student.hcmus.edu.vn>
2020-07-14 01:03:10 +0700

    [SERVER] implement BookingMapper (cont.)

--- service/booking-service/src/main/java/com/bht/saigonparking/service/booking/mapper/BookingMapper.java
+++ service/booking-service/src/main/java/com/bht/saigonparking/service/booking/mapper/BookingMapper.java
@@ -9,8 +9,10 @@ import org.mapstruct.NullValueMappingStrategy;
 import org.springframework.stereotype.Component;
 
 import com.bht.saigonparking.api.grpc.booking.Booking;
+import com.bht.saigonparking.api.grpc.booking.BookingHistory;
 import com.bht.saigonparking.service.booking.configuration.AppConfiguration;
 import com.bht.saigonparking.service.booking.entity.BookingEntity;
+import com.bht.saigonparking.service.booking.entity.BookingHistoryEntity;
 
 /**
  *
@@ -33,11 +35,19 @@ public interface BookingMapper {
     Booking toBooking(@NotNull BookingEntity bookingEntity);
 
     @Named("toBookingEntity")
-    @Mapping(target = "id", ignore = true)
     @Mapping(target = "parkingLotId", source = "parkingLotId", defaultExpression = "java(customizedMapper.DEFAULT_LONG_VALUE)")
     @Mapping(target = "customerId", source = "customerId", defaultExpression = "java(customizedMapper.DEFAULT_LONG_VALUE)")
     @Mapping(target = "customerLicensePlate", source = "licensePlate", defaultExpression = "java(customizedMapper.DEFAULT_STRING_VALUE)")
+    @Mapping(target = "id", ignore = true)
     @Mapping(target = "isFinished", ignore = true)
     @Mapping(target = "version", ignore = true)
     BookingEntity toBookingEntity(@NotNull Booking booking);
+
+    @Named("toBookingHistory")
+    @Mapping(target = "id", source = "id", defaultExpression = "java(customizedMapper.DEFAULT_LONG_VALUE)")
+    @Mapping(target = "bookingId", source = "bookingEntity.id", defaultExpression = "java(customizedMapper.DEFAULT_LONG_VALUE)")
+    @Mapping(target = "status", source = "bookingStatusEntity", qualifiedByName = "toBookingStatus")
+    @Mapping(target = "timestamp", source = "lastUpdated", qualifiedByName = "toTimestampString", defaultExpression = "java(customizedMapper.DEFAULT_STRING_VALUE)")
+    @Mapping(target = "note", source = "note", defaultExpression = "java(customizedMapper.DEFAULT_STRING_VALUE)")
+    BookingHistory toBookingHistory(@NotNull BookingHistoryEntity bookingHistoryEntity);
 }
