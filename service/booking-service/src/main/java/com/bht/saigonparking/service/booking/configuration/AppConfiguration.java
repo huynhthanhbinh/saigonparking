3b34a68b1f4f7e1c35105c36432af8fbbd2a1f2e
Huynh Thanh Binh <1653006@student.hcmus.edu.vn>
2020-07-13 20:47:37 +0700

    [SERVER] init BookingService configurations

---
+++ service/booking-service/src/main/java/com/bht/saigonparking/service/booking/configuration/AppConfiguration.java
@@ -0,0 +1,68 @@
+package com.bht.saigonparking.service.booking.configuration;
+
+import javax.naming.ServiceUnavailableException;
+import javax.persistence.EntityNotFoundException;
+
+import org.lognet.springboot.grpc.GRpcGlobalInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.bht.saigonparking.common.annotation.InheritedComponent;
+import com.bht.saigonparking.common.exception.PermissionDeniedException;
+import com.bht.saigonparking.common.interceptor.SaigonParkingClientInterceptor;
+import com.bht.saigonparking.common.interceptor.SaigonParkingServerInterceptor;
+import com.bht.saigonparking.common.spring.SpringApplicationContext;
+import com.bht.saigonparking.common.spring.SpringBeanLifeCycle;
+import com.google.common.collect.ImmutableMap;
+
+/**
+ *
+ * @author bht
+ */
+@Configuration
+@EnableTransactionManagement
+@ComponentScan(basePackages = AppConfiguration.BASE_PACKAGE,
+        includeFilters = @ComponentScan.Filter(InheritedComponent.class))
+public class AppConfiguration {
+
+    public static final String BASE_PACKAGE = "com.bht.saigonparking.service.booking";
+
+    @Bean
+    public SpringApplicationContext springApplicationContext() {
+        return new SpringApplicationContext();
+    }
+
+    @Bean
+    public SpringBeanLifeCycle springBeanLifeCycle() {
+        return new SpringBeanLifeCycle(BASE_PACKAGE);
+    }
+
+    @Bean
+    public ProtobufHttpMessageConverter protobufHttpMessageConverter() {
+        return new ProtobufHttpMessageConverter();
+    }
+
+    @Bean
+    public SaigonParkingClientInterceptor saigonParkingClientInterceptor() {
+        return new SaigonParkingClientInterceptor(SaigonParkingClientInterceptor.INTERNAL_CODE_BOOKING_SERVICE);
+    }
+
+    @Bean
+    @GRpcGlobalInterceptor
+    public SaigonParkingServerInterceptor saigonParkingServerInterceptor() {
+        return new SaigonParkingServerInterceptor(new ImmutableMap.Builder<Class<? extends Throwable>, String>()
+                .put(EntityNotFoundException.class, "SPE#00008")
+                .put(DataIntegrityViolationException.class, "SPE#00009")
+                .put(PermissionDeniedException.class, "SPE#00015")
+                .put(ObjectOptimisticLockingFailureException.class, "SPE#00016")
+                .put(ServiceUnavailableException.class, "SPE#00017")
+                .put(EmptyResultDataAccessException.class, "SPE#00018")
+                .build());
+    }
+}
