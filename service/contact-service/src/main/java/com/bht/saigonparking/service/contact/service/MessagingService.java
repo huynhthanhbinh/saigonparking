6a78e97b161522d346952eeb0a09e0d166649a59
Huynh Thanh Binh <1653006@student.hcmus.edu.vn>
2020-07-16 03:31:09 +0700

    [SERVER] Contact service: init preProcessingMessage() (cont.)

--- service/contact-service/src/main/java/com/bht/saigonparking/service/contact/service/MessagingService.java
+++ service/contact-service/src/main/java/com/bht/saigonparking/service/contact/service/MessagingService.java
@@ -1,11 +1,12 @@
 package com.bht.saigonparking.service.contact.service;
 
+import java.io.IOException;
+
 import javax.validation.constraints.NotNull;
 
 import org.springframework.web.socket.WebSocketSession;
 
 import com.bht.saigonparking.api.grpc.contact.SaigonParkingMessage;
-import com.google.protobuf.InvalidProtocolBufferException;
 
 /**
  *
@@ -14,7 +15,7 @@ import com.google.protobuf.InvalidProtocolBufferException;
 public interface MessagingService {
 
     SaigonParkingMessage.Builder prePublishMessageToQueue(@NotNull SaigonParkingMessage.Builder delegate,
-                                                          @NotNull WebSocketSession webSocketSession) throws InvalidProtocolBufferException;
+                                                          @NotNull WebSocketSession webSocketSession) throws IOException;
 
     void publishMessageToQueue(@NotNull SaigonParkingMessage saigonParkingMessage);
 
--- service/contact-service/src/main/java/com/bht/saigonparking/service/contact/service/impl/MessagingServiceImpl.java
+++ service/contact-service/src/main/java/com/bht/saigonparking/service/contact/service/impl/MessagingServiceImpl.java
@@ -1,5 +1,8 @@
 package com.bht.saigonparking.service.contact.service.impl;
 
+import static com.bht.saigonparking.api.grpc.contact.SaigonParkingMessage.Classification.SYSTEM_MESSAGE;
+import static com.bht.saigonparking.api.grpc.contact.SaigonParkingMessage.Type.BOOKING_PROCESSING;
+
 import java.io.IOException;
 import java.util.Set;
 
@@ -12,9 +15,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.WebSocketSession;
 
+import com.bht.saigonparking.api.grpc.booking.BookingServiceGrpc;
+import com.bht.saigonparking.api.grpc.booking.BookingStatus;
+import com.bht.saigonparking.api.grpc.booking.CreateBookingRequest;
+import com.bht.saigonparking.api.grpc.booking.UpdateBookingStatusRequest;
 import com.bht.saigonparking.api.grpc.contact.BookingAcceptanceContent;
 import com.bht.saigonparking.api.grpc.contact.BookingCancellationContent;
 import com.bht.saigonparking.api.grpc.contact.BookingFinishContent;
+import com.bht.saigonparking.api.grpc.contact.BookingProcessingContent;
 import com.bht.saigonparking.api.grpc.contact.BookingRejectContent;
 import com.bht.saigonparking.api.grpc.contact.BookingRequestContent;
 import com.bht.saigonparking.api.grpc.contact.SaigonParkingMessage;
@@ -22,8 +30,9 @@ import com.bht.saigonparking.common.constant.SaigonParkingMessageQueue;
 import com.bht.saigonparking.common.util.LoggingUtil;
 import com.bht.saigonparking.service.contact.handler.WebSocketUserSessionManagement;
 import com.bht.saigonparking.service.contact.service.MessagingService;
-import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.Empty;
 
+import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -36,10 +45,12 @@ public final class MessagingServiceImpl implements MessagingService {
 
     private final RabbitTemplate rabbitTemplate;
     private final WebSocketUserSessionManagement webSocketUserSessionManagement;
+    private final BookingServiceGrpc.BookingServiceStub bookingServiceStub;
+    private final BookingServiceGrpc.BookingServiceBlockingStub bookingServiceBlockingStub;
 
     @Override
     public SaigonParkingMessage.Builder prePublishMessageToQueue(@NotNull SaigonParkingMessage.Builder delegate,
-                                                                 @NotNull WebSocketSession webSocketSession) throws InvalidProtocolBufferException {
+                                                                 @NotNull WebSocketSession webSocketSession) throws IOException {
         switch (delegate.getClassification()) {
             case CUSTOMER_MESSAGE:
                 delegate.setSenderId(webSocketUserSessionManagement.getUserIdFromSession(webSocketSession));
@@ -107,22 +118,125 @@ public final class MessagingServiceImpl implements MessagingService {
     }
 
     private SaigonParkingMessage.Builder preProcessingMessage(@NotNull SaigonParkingMessage.Builder delegate,
-                                                              @NotNull WebSocketSession webSocketSession) throws InvalidProtocolBufferException {
+                                                              @NotNull WebSocketSession webSocketSession) throws IOException {
         switch (delegate.getType()) {
             case BOOKING_REQUEST:
                 BookingRequestContent bookingRequestContent = BookingRequestContent.parseFrom(delegate.getContent());
+                long newBookingId = bookingServiceBlockingStub.createBooking(CreateBookingRequest.newBuilder()
+                        .setParkingLotId(bookingRequestContent.getParkingLotId())
+                        .setCustomerId(delegate.getSenderId())
+                        .setLicensePlate(bookingRequestContent.getCustomerLicense())
+                        .build())
+                        .getValue();
+
+                BookingProcessingContent bookingProcessingContent = BookingProcessingContent.newBuilder()
+                        .setParkingLotId(bookingRequestContent.getParkingLotId())
+                        .setBookingId(newBookingId)
+                        .build();
+
+                SaigonParkingMessage bookingProcessingMessage = SaigonParkingMessage.newBuilder()
+                        .setSenderId(0)
+                        .setReceiverId(delegate.getSenderId())
+                        .setClassification(SYSTEM_MESSAGE)
+                        .setType(BOOKING_PROCESSING)
+                        .setContent(bookingProcessingContent.toByteString())
+                        .build();
+
+                webSocketSession.sendMessage(new BinaryMessage(bookingProcessingMessage.toByteArray()));
                 break;
             case BOOKING_CANCELLATION:
                 BookingCancellationContent bookingCancellationContent = BookingCancellationContent.parseFrom(delegate.getContent());
+                bookingServiceStub.updateBookingStatus(UpdateBookingStatusRequest.newBuilder()
+                        .setBookingId(bookingCancellationContent.getBookingId())
+                        .setStatus(BookingStatus.CANCELLED)
+                        .setNote(bookingCancellationContent.getReason())
+                        .setTimestamp(delegate.getTimestamp())
+                        .build(), new StreamObserver<Empty>() {
+                    @Override
+                    public void onNext(Empty empty) {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+
+                    }
+
+                    @Override
+                    public void onCompleted() {
+
+                    }
+                });
                 break;
             case BOOKING_ACCEPTANCE:
                 BookingAcceptanceContent bookingAcceptanceContent = BookingAcceptanceContent.parseFrom(delegate.getContent());
+                bookingServiceStub.updateBookingStatus(UpdateBookingStatusRequest.newBuilder()
+                        .setBookingId(bookingAcceptanceContent.getBookingId())
+                        .setStatus(BookingStatus.ACCEPTED)
+                        .setTimestamp(delegate.getTimestamp())
+                        .build(), new StreamObserver<Empty>() {
+                    @Override
+                    public void onNext(Empty empty) {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+
+                    }
+
+                    @Override
+                    public void onCompleted() {
+
+                    }
+                });
                 break;
             case BOOKING_REJECT:
                 BookingRejectContent bookingRejectContent = BookingRejectContent.parseFrom(delegate.getContent());
+                bookingServiceStub.updateBookingStatus(UpdateBookingStatusRequest.newBuilder()
+                        .setBookingId(bookingRejectContent.getBookingId())
+                        .setStatus(BookingStatus.REJECTED)
+                        .setNote(bookingRejectContent.getReason())
+                        .setTimestamp(delegate.getTimestamp())
+                        .build(), new StreamObserver<Empty>() {
+                    @Override
+                    public void onNext(Empty empty) {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+
+                    }
+
+                    @Override
+                    public void onCompleted() {
+
+                    }
+                });
                 break;
             case BOOKING_FINISH:
                 BookingFinishContent bookingFinishContent = BookingFinishContent.parseFrom(delegate.getContent());
+                bookingServiceStub.updateBookingStatus(UpdateBookingStatusRequest.newBuilder()
+                        .setBookingId(bookingFinishContent.getBookingId())
+                        .setStatus(BookingStatus.FINISHED)
+                        .setTimestamp(delegate.getTimestamp())
+                        .build(), new StreamObserver<Empty>() {
+                    @Override
+                    public void onNext(Empty empty) {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+
+                    }
+
+                    @Override
+                    public void onCompleted() {
+
+                    }
+                });
                 break;
             default:
                 break;
