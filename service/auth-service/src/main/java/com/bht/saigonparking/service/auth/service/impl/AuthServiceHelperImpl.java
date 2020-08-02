d93134032781ccda78f11083565602e7e449ed80
Huynh Thanh Binh <1653006@student.hcmus.edu.vn>
2020-08-02 20:33:38 +0700

    [SERVER] update AuthService using @Async on class-level

--- service/auth-service/src/main/java/com/bht/saigonparking/service/auth/service/impl/AuthServiceHelperImpl.java
+++ service/auth-service/src/main/java/com/bht/saigonparking/service/auth/service/impl/AuthServiceHelperImpl.java
@@ -3,27 +3,20 @@ package com.bht.saigonparking.service.auth.service.impl;
 import static com.bht.saigonparking.common.constant.SaigonParkingMessageQueue.MAIL_TOPIC_ROUTING_KEY;
 import static com.bht.saigonparking.common.constant.SaigonParkingMessageQueue.USER_TOPIC_ROUTING_KEY;
 
-import java.util.Date;
-
 import javax.persistence.EntityNotFoundException;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-import org.apache.commons.lang3.tuple.Triple;
 import org.apache.logging.log4j.Level;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.util.Pair;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.bht.saigonparking.api.grpc.mail.MailRequest;
 import com.bht.saigonparking.api.grpc.mail.MailRequestType;
 import com.bht.saigonparking.api.grpc.user.UpdateUserLastSignInRequest;
-import com.bht.saigonparking.api.grpc.user.User;
 import com.bht.saigonparking.api.grpc.user.UserServiceGrpc;
-import com.bht.saigonparking.common.auth.SaigonParkingAuthentication;
-import com.bht.saigonparking.common.exception.InvalidRefreshTokenException;
 import com.bht.saigonparking.common.util.LoggingUtil;
 import com.bht.saigonparking.service.auth.entity.UserTokenEntity;
 import com.bht.saigonparking.service.auth.repository.UserTokenRepository;
@@ -37,25 +30,23 @@ import lombok.AllArgsConstructor;
  *
  * @author bht
  */
+@Async
 @Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
-public class AuthServiceImplHelper {
+public class AuthServiceHelperImpl {
 
-    private final SaigonParkingAuthentication authentication;
     private final UserTokenRepository userTokenRepository;
     private final RabbitTemplate rabbitTemplate;
     private final UserServiceGrpc.UserServiceStub userServiceStub;
 
-    @Async
-    void updateUserLastSignIn(@NotNull Long userId) {
+    public void updateUserLastSignIn(@NotNull Long userId) {
         rabbitTemplate.convertAndSend(USER_TOPIC_ROUTING_KEY, UpdateUserLastSignInRequest.newBuilder()
                 .setUserId(userId)
                 .setTimeInMillis(System.currentTimeMillis())
                 .build());
     }
 
-    @Async
-    void activateUserWithId(@NotNull Long userId) {
+    public void activateUserWithId(@NotNull Long userId) {
         userServiceStub.activateUser(Int64Value.of(userId), new StreamObserver<Empty>() {
             @Override
             public void onNext(Empty empty) {
@@ -75,11 +66,10 @@ public class AuthServiceImplHelper {
         });
     }
 
-    @Async
-    void sendMail(@NotNull MailRequestType type,
-                  @NotEmpty String email,
-                  @NotEmpty String username,
-                  @NotEmpty String temporaryToken) {
+    public void sendMail(@NotNull MailRequestType type,
+                         @NotEmpty String email,
+                         @NotEmpty String username,
+                         @NotEmpty String temporaryToken) {
 
         rabbitTemplate.convertAndSend(MAIL_TOPIC_ROUTING_KEY, MailRequest.newBuilder()
                 .setType(type)
@@ -89,50 +79,22 @@ public class AuthServiceImplHelper {
                 .build());
     }
 
-    @Async
     public void saveUserRefreshToken(@NotNull Long userId, @NotEmpty String tokenId) {
         try {
+            /* update refresh token id to database if already existed */
             UserTokenEntity userTokenEntity = userTokenRepository.findById(userId).orElseThrow(EntityNotFoundException::new);
             userTokenEntity.setTokenId(tokenId);
             userTokenRepository.saveAndFlush(userTokenEntity);
 
         } catch (EntityNotFoundException entityNotFoundException) {
 
+            /* save new refresh token id to database if not existed before */
             UserTokenEntity userTokenEntity = UserTokenEntity.builder().userId(userId).tokenId(tokenId).build();
             userTokenRepository.saveAndFlush(userTokenEntity);
 
         } finally {
             LoggingUtil.log(Level.INFO, "SERVICE", "Success",
                     String.format("saveUserRefreshToken(%d, %s)", userId, tokenId));
-        }
-    }
-
-    Triple<String, String, String> generateNewToken(@NotNull User user,
-                                                    @NotNull Date currentExp,
-                                                    @NotEmpty String currentTokenId,
-                                                    boolean currentIsRefreshToken) {
-
-        try {
-            if (currentIsRefreshToken) {
-                userTokenRepository.findByTokenId(currentTokenId).orElseThrow(EntityNotFoundException::new);
-                userTokenRepository.flush();
-            }
-        } catch (EntityNotFoundException entityNotFoundException) {
-            throw new InvalidRefreshTokenException();
-        }
-
-        Pair<String, String> generatedAccessToken = authentication.generateAccessToken(user.getId(), user.getRole().toString());
-
-        if (((currentExp.getTime() - new Date().getTime()) / 86400000) > 7) { /* Token not nearly expire */
-            return Triple.of(user.getUsername(), generatedAccessToken.getSecond(), "");
-
-        } else { /* Token nearly expire */
-
-            /* Generate new refresh token for user with Id, Role */
-            Pair<String, String> generatedRefreshToken = authentication.generateRefreshToken(user.getId(), user.getRole().toString());
-            saveUserRefreshToken(user.getId(), generatedRefreshToken.getFirst());
-
-            return Triple.of(user.getUsername(), generatedAccessToken.getSecond(), generatedRefreshToken.getSecond());
         }
     }
 }
--- service/auth-service/src/main/java/com/bht/saigonparking/service/auth/service/impl/AuthServiceImpl.java
+++ service/auth-service/src/main/java/com/bht/saigonparking/service/auth/service/impl/AuthServiceImpl.java
@@ -5,6 +5,7 @@ import static com.bht.saigonparking.api.grpc.mail.MailRequestType.RESET_PASSWORD
 
 import java.util.Date;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
@@ -21,10 +22,12 @@ import com.bht.saigonparking.api.grpc.user.User;
 import com.bht.saigonparking.api.grpc.user.UserRole;
 import com.bht.saigonparking.api.grpc.user.UserServiceGrpc;
 import com.bht.saigonparking.common.auth.SaigonParkingAuthentication;
+import com.bht.saigonparking.common.exception.InvalidRefreshTokenException;
 import com.bht.saigonparking.common.exception.UserAlreadyActivatedException;
 import com.bht.saigonparking.common.exception.UserNotActivatedException;
 import com.bht.saigonparking.common.exception.WrongPasswordException;
 import com.bht.saigonparking.common.exception.WrongUserRoleException;
+import com.bht.saigonparking.service.auth.repository.UserTokenRepository;
 import com.bht.saigonparking.service.auth.service.AuthService;
 import com.google.protobuf.Int64Value;
 import com.google.protobuf.StringValue;
@@ -41,8 +44,9 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class AuthServiceImpl implements AuthService {
 
+    private final UserTokenRepository userTokenRepository;
     private final SaigonParkingAuthentication authentication;
-    private final AuthServiceImplHelper authServiceImplHelper;
+    private final AuthServiceHelperImpl authServiceImplHelper;
     private final UserServiceGrpc.UserServiceBlockingStub userServiceBlockingStub;
 
     @Override
@@ -51,6 +55,7 @@ public class AuthServiceImpl implements AuthService {
                                               @NotNull UserRole userRole) {
 
         User user = userServiceBlockingStub.getUserByUsername(StringValue.of(username));
+
         if (user.getRole().equals(userRole)) {
             if (Boolean.TRUE.equals(user.getIsActivated())) {
                 if (BCrypt.checkpw(password, user.getPassword())) {
@@ -136,7 +141,7 @@ public class AuthServiceImpl implements AuthService {
 
         User user = userServiceBlockingStub.getUserById(Int64Value.of(userId));
 
-        return authServiceImplHelper.generateNewToken(user, currentExp, currentTokenId, currentIsRefreshToken);
+        return generateNewToken(user, currentExp, currentTokenId, currentIsRefreshToken);
     }
 
     @Override
@@ -151,6 +156,35 @@ public class AuthServiceImpl implements AuthService {
         Context context = Context.current().fork();
         context.run(() -> authServiceImplHelper.activateUserWithId(userId));
 
-        return authServiceImplHelper.generateNewToken(user, currentExp, currentTokenId, currentIsRefreshToken);
+        return generateNewToken(user, currentExp, currentTokenId, currentIsRefreshToken);
+    }
+
+    @SuppressWarnings("java:S2201")
+    private Triple<String, String, String> generateNewToken(@NotNull User user,
+                                                            @NotNull Date currentExp,
+                                                            @NotEmpty String currentTokenId,
+                                                            boolean currentIsRefreshToken) {
+        try {
+            if (currentIsRefreshToken) {
+                userTokenRepository.findByTokenId(currentTokenId).orElseThrow(EntityNotFoundException::new);
+                userTokenRepository.flush();
+            }
+        } catch (EntityNotFoundException entityNotFoundException) {
+            throw new InvalidRefreshTokenException();
+        }
+
+        Pair<String, String> generatedAccessToken = authentication.generateAccessToken(user.getId(), user.getRole().toString());
+
+        if (((currentExp.getTime() - new Date().getTime()) / 86400000) > 7) { /* Token not nearly expire */
+            return Triple.of(user.getUsername(), generatedAccessToken.getSecond(), "");
+
+        } else { /* Token nearly expire */
+
+            /* Generate new refresh token for user with Id, Role */
+            Pair<String, String> generatedRefreshToken = authentication.generateRefreshToken(user.getId(), user.getRole().toString());
+            authServiceImplHelper.saveUserRefreshToken(user.getId(), generatedRefreshToken.getFirst());
+
+            return Triple.of(user.getUsername(), generatedAccessToken.getSecond(), generatedRefreshToken.getSecond());
+        }
     }
 }
