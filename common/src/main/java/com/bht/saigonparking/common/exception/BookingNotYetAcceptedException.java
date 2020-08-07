5686b74db8dd32dcbd95474fe7d5569b7d0a8102
Huynh Thanh Binh <1653006@student.hcmus.edu.vn>
2020-08-07 21:43:33 +0700

    [SERVER] add BookingNotYetAcceptedException

---
+++ common/src/main/java/com/bht/saigonparking/common/exception/BookingNotYetAcceptedException.java
@@ -0,0 +1,12 @@
+package com.bht.saigonparking.common.exception;
+
+import com.bht.saigonparking.common.base.BaseRuntimeException;
+
+/**
+ *
+ * This exception will be thrown if booking not yet accepted but want to finish it
+ *
+ * @author bht
+ */
+public final class BookingNotYetAcceptedException extends BaseRuntimeException {
+}
