0ea9807970555253343697adb7da8b900daccdd5
Huynh Thanh Binh <1653006@student.hcmus.edu.vn>
2020-08-24 18:41:11 +0700

    [SERVER] add some Booking rating exceptions

---
+++ common/src/main/java/com/bht/saigonparking/common/exception/BookingAlreadyRatedException.java
@@ -0,0 +1,12 @@
+package com.bht.saigonparking.common.exception;
+
+import com.bht.saigonparking.common.base.BaseRuntimeException;
+
+/**
+ *
+ * This exception will be thrown if booking already rated but want to create new rating
+ *
+ * @author bht
+ */
+public final class BookingAlreadyRatedException extends BaseRuntimeException {
+}
---
+++ common/src/main/java/com/bht/saigonparking/common/exception/BookingNotYetRatedException.java
@@ -0,0 +1,12 @@
+package com.bht.saigonparking.common.exception;
+
+import com.bht.saigonparking.common.base.BaseRuntimeException;
+
+/**
+ *
+ * This exception will be thrown if booking not yet rated but want to modify rating
+ *
+ * @author bht
+ */
+public final class BookingNotYetRatedException extends BaseRuntimeException {
+}
